5d4ffb3bab2bb5be733b11aa8c67da5b15a2feff
Ivann03 <sentiv@DESKTOP-1DF5IGF.localdomain>
2023-05-29 01:35:55 +0700

    Исправил метод GetPos, который используется для получения позиции пина на основе родительского объекта

---
+++ LogicSimulator/Models/Locontrol.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using LogicSimulator.Views.Shapes;
+
+namespace LogicSimulator.Models {
+    public class Locontrol {
+        public readonly int num;
+        public Func parent;
+        public readonly string tag;
+
+        public Locontrol(Func parent, int n, string tag) { 
+            this.parent = parent;
+            num = n; 
+            this.tag = tag;
+        }
+
+        public Point GetPos() => parent.GetPinPos(num);
+    }
+}
